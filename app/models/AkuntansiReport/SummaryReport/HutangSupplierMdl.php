f3acdc7756e323b072255bed3a9b4b8773771971
rhmthid <rhmthid@gmail.com>
2026-04-08 20:29:42 +0700

    Update Summary A/P Purchasing Part 2

--- app/models/AkuntansiReport/SummaryReport/HutangSupplierMdl.php
+++ app/models/AkuntansiReport/SummaryReport/HutangSupplierMdl.php
@@ -23,20 +23,20 @@ class HutangSupplierMdl extends DB
 
         $addsql = $addsql2 = "";
         $bid = $data['bid'];
-        $status_cabang = $data['status_cabang'];
-        $jurnal_speriod = $data['jurnal_speriod'];
-        $jurnal_eperiod = $data['jurnal_eperiod'];
-        $bid  = $data['bid'];
-        $is_posted = $data['is_posted'];
-        $gldoc = strtolower(trim($data['gldoc']));
-        $keterangan = strtolower(trim($data['keterangan']));
+        // $status_cabang = $data['status_cabang'];
+        // $jurnal_speriod = $data['jurnal_speriod'];
+        // $jurnal_eperiod = $data['jurnal_eperiod'];
+        // $bid  = $data['bid'];
+        // $is_posted = $data['is_posted'];
+        // $gldoc = strtolower(trim($data['gldoc']));
+        // $keterangan = strtolower(trim($data['keterangan']));
         $record = [];
         $optionsCabang = FilterCabang($bid);
 
         /* B: Create Temp Table */
-        DB::Execute("DROP TABLE IF EXISTS temp_general_ledger");
+        DB::Execute("DROP TABLE IF EXISTS temp_summary_ap");
 
-        $sqli = "CREATE TEMPORARY TABLE temp_general_ledger (
+        $sqli = "CREATE TEMPORARY TABLE temp_summary_ap (
                     branch_code VARCHAR(255),
                     glid BIGINT,
                     gldate TIMESTAMP,
@@ -48,18 +48,12 @@ class HutangSupplierMdl extends DB
                 );";
         DB::Execute($sqli);
 
-        DB::Execute("CREATE INDEX idx_temp_glid ON temp_general_ledger(glid);");
+        DB::Execute("CREATE INDEX idx_temp_glid ON temp_summary_ap(glid);");
         /* E: Create Temp Table */
 
         /* B: Get Data PT. JKK */
         if ($optionsCabang['conn_jkk'])
         {
-            if ($is_posted) $addsql .= " AND a.is_posted = '$is_posted'";
-
-            if ($gldoc) $addsql .= " AND format_glcode(a.gldate, b.doc_code, a.gldoc, a.glid) = '$gldoc'";
-
-            if ($keterangan) $addsql .= " AND LOWER(a.keterangan) LIKE '%$keterangan%'";
-
             $sql = "SELECT d.branch_code, a.glid, a.gldate, a.gldesc, c.nama_lengkap AS useri
                         , format_glcode(a.gldate, b.doc_code, a.gldoc, a.glid) AS gldoc
                         , b.journal_name, a.is_posted
@@ -92,12 +86,6 @@ class HutangSupplierMdl extends DB
         /* B: Get Data PT. KAH */
         if ($optionsCabang['conn_kah'])
         {
-            if ($is_posted) $addsql .= " AND a.is_posted = '$is_posted'";
-
-            if ($gldoc) $addsql .= " AND format_glcode(a.gldate, b.doc_code, a.gldoc, a.glid) = '$gldoc'";
-
-            if ($keterangan) $addsql .= " AND LOWER(a.keterangan) LIKE '%$keterangan%'";
-
             $sql = "SELECT a.glid, a.gldate, a.gldesc, c.nama_lengkap AS useri
                         , format_glcode(a.gldate, b.doc_code, a.gldoc, a.glid) AS gldoc
                         , b.journal_name, a.is_posted
@@ -149,7 +137,7 @@ class HutangSupplierMdl extends DB
                     'is_posted'     => $row['is_posted']
                 );
 
-                $sqli = "SELECT * FROM temp_general_ledger WHERE 1 = 2";
+                $sqli = "SELECT * FROM temp_summary_ap WHERE 1 = 2";
                 $rsi = DB::Execute($sqli);
                 $sqli = DB::InsertSQL($rsi, $data);
                 if ($ok) $ok = DB::Execute($sqli);
@@ -163,7 +151,7 @@ class HutangSupplierMdl extends DB
 
         /* B: Showing Data From Temp Table */
         $sql = "SELECT tmp.*, br.bid, br.branch_name
-                FROM temp_general_ledger tmp
+                FROM temp_summary_ap tmp
                 INNER JOIN branch br ON br.branch_code = tmp.branch_code
                 WHERE 1 = 1 $addsql2
                 ORDER BY tmp.gldate DESC, tmp.glid DESC";
@@ -172,202 +160,6 @@ class HutangSupplierMdl extends DB
         else $sqlx = $sql;
 
         $rs = DB::Execute($sqlx);
-        /* E: Showing Data From Temp Table */
-
-        return $rs;
-    } /*}}}*/
-
-    public static function detail_jurnal ($myglid, $mybid) /*{{{*/
-    {
-        // if (Auth::user()->pid == SUPER_USER) DB::Debug(true);
-
-        $addsql = "";
-        $optionsCabang = FilterCabang($bid);
-        $record = [];
-        $bcode = DB::GetOne("SELECT branch_code FROM branch WHERE bid = ?", [$mybid]);
-
-        /* B: Create Temp Table */
-        DB::Execute("DROP TABLE IF EXISTS temp_detail_jurnal");
-
-        $sqli = "CREATE TEMPORARY TABLE temp_detail_jurnal (
-                    branch_code VARCHAR,
-                    gldid BIGINT,
-                    glid BIGINT,
-                    gldesc TEXT,
-                    gldate TIMESTAMP,
-                    journal_name VARCHAR(255),
-                    create_time TIMESTAMP,
-                    gldoc VARCHAR(255),
-                    posted VARCHAR(50),
-                    coacode VARCHAR(50),
-                    coaname VARCHAR(255),
-                    debet NUMERIC(18, 2),
-                    credit NUMERIC(18, 2),
-                    notes TEXT,
-                    posted_by VARCHAR(255),
-                    supp_cust VARCHAR(255),
-                    reff_code VARCHAR(100),
-                    cost_center VARCHAR(255)
-                );";
-        DB::Execute($sqli);
-
-        DB::Execute("CREATE INDEX idx_temp_detail_gldid ON temp_detail_jurnal(gldid);");
-        /* E: Create Temp Table */
-
-        /* B: Get Data PT. JKK */
-        if ($optionsCabang['conn_jkk'])
-        {
-            $sql = "SELECT br.branch_code, a.gldid, b.glid, b.gldesc, b.gldate, b.is_posted, d.journal_name, b.create_time
-                        , format_glcode(b.gldate, d.doc_code, b.gldoc, b.glid) AS gldoc
-                        , (CASE WHEN b.is_posted THEN 'POSTED' ELSE 'NOT POSTED' END) AS posted
-                        , b.jtid, c.coacode, c.coaname, a.debet, a.credit, a.notes, e.nama_lengkap AS posted_by
-                        , (CASE WHEN b.jtid IN (4, 9, 20, 21, 22, 23, 24, 25, 26, 29) 
-                            THEN f.nama_supp 
-                        WHEN b.jtid IN (27,28) AND b.suppid = -1  -- PIUTANG PEGAWAI
-                            THEN j.nama_lengkap 
-                        WHEN b.jtid IN (27,28) AND b.suppid = -2  -- PIUTANG EDC
-                            THEN i.bank_nama 
-                        ELSE g.nama_customer END) AS supp_cust
-                        , b.reff_code, (h.pcccode || ' - ' || h.pccname) AS cost_center
-                    FROM general_ledger_d a
-                    INNER JOIN general_ledger b ON b.glid = a.glid
-                    INNER JOIN m_coa c ON c.coaid = a.coaid
-                    INNER JOIN journal_type d ON d.jtid = b.jtid
-                    INNER JOIN person e ON e.pid = b.create_by
-                    LEFT JOIN m_supplier f ON b.suppid = f.suppid
-                    LEFT JOIN m_customer g ON b.suppid = g.custid
-                    LEFT JOIN profit_cost_center h ON a.pccid = h.pccid
-                    LEFT JOIN m_bank i ON b.other_reff_id = i.bank_id
-                    LEFT JOIN person j ON b.other_reff_id = j.pid
-                    INNER JOIN branch br ON br.bid = b.bid
-                    WHERE b.glid = ? AND br.branch_code = ?";
-            $rs = DB2::Execute($sql, [$myglid, $bcode]);
-
-            while (!$rs->EOF)
-            {
-                $record[] = array(
-                    'branch_code'   => $rs->fields['branch_code'],
-                    'gldid'         => $rs->fields['gldid'],
-                    'glid'          => $rs->fields['glid'],
-                    'gldesc'        => $rs->fields['gldesc'],
-                    'gldate'        => $rs->fields['gldate'],
-                    'posted'        => $rs->fields['posted'],
-                    'journal_name'  => $rs->fields['journal_name'],
-                    'create_time'   => $rs->fields['create_time'],
-                    'gldoc'         => $rs->fields['gldoc'],
-                    'coacode'       => $rs->fields['coacode'],
-                    'coaname'       => $rs->fields['coaname'],
-                    'debet'         => floatval($rs->fields['debet']),
-                    'credit'        => floatval($rs->fields['credit']),
-                    'notes'         => $rs->fields['notes'],
-                    'posted_by'     => $rs->fields['posted_by'],
-                    'supp_cust'     => $rs->fields['supp_cust'],
-                    'reff_code'     => $rs->fields['reff_code'],
-                    'cost_center'   => $rs->fields['cost_center'],
-                );
-
-                $rs->MoveNext();
-            }
-        }
-        /* E: Get Data PT. JKK */
-
-        /* B: Get Data PT. KAH */
-        if ($optionsCabang['conn_kah'])
-        {
-            $sql = "SELECT a.gldid, b.glid, b.gldesc, b.gldate, b.is_posted, d.journal_name, b.create_time
-                        ,format_glcode(b.gldate, d.doc_code, b.gldoc, b.glid) AS gldoc, b.jtid
-                        , (CASE WHEN b.is_posted THEN 'POSTED' ELSE 'NOT POSTED' END) AS posted, b.reff_code
-                        , c.coacode, c.coaname, a.debet, a.credit, a.notes, e.nama_lengkap AS posted_by
-                        , (CASE WHEN b.jtid IN (4, 9, 20, 21, 22, 23, 24) THEN f.nama_supp ELSE g.nama_customer END) AS supp_cust
-                    FROM general_ledger_d a
-                    INNER JOIN general_ledger b ON b.glid = a.glid
-                    INNER JOIN m_coa c ON c.coaid = a.coaid
-                    INNER JOIN journal_type d ON d.jtid = b.jtid
-                    INNER JOIN person e ON e.pid = b.create_by
-                    LEFT JOIN m_supplier f ON b.suppid = f.suppid
-                    LEFT JOIN m_customer g ON b.suppid = g.custid
-                    WHERE b.glid = ?
-                    ORDER BY a.gldid";
-            $rs = DB3::Execute($sql, [$myglid]);
-
-            while (!$rs->EOF)
-            {
-                $record[] = array(
-                    'branch_code'   => self::$kode_kah,
-                    'gldid'         => $rs->fields['gldid'],
-                    'glid'          => $rs->fields['glid'],
-                    'gldesc'        => $rs->fields['gldesc'],
-                    'gldate'        => $rs->fields['gldate'],
-                    'posted'        => $rs->fields['posted'],
-                    'journal_name'  => $rs->fields['journal_name'],
-                    'create_time'   => $rs->fields['create_time'],
-                    'gldoc'         => $rs->fields['gldoc'],
-                    'coacode'       => $rs->fields['coacode'],
-                    'coaname'       => $rs->fields['coaname'],
-                    'debet'         => floatval($rs->fields['debet']),
-                    'credit'        => floatval($rs->fields['credit']),
-                    'notes'         => $rs->fields['notes'],
-                    'posted_by'     => $rs->fields['posted_by'],
-                    'supp_cust'     => $rs->fields['supp_cust'],
-                    'reff_code'     => $rs->fields['reff_code'],
-                    'cost_center'   => $rs->fields['cost_center'],
-                );
-
-                $rs->MoveNext();
-            }
-        }
-        /* E: Get Data PT. KAH */
-
-        /* B: Get Data RSJK */
-        if ($optionsCabang['conn_rsjk'])
-        {
-        }
-        /* E: Get Data RSJK */
-
-        /* B: Insert To Temp Table */
-        $ok = true;
-        if (!empty($record))
-        {
-            foreach ($record as $idx => $row)
-            {
-                $data = array(
-                    'branch_code'   => $row['branch_code'],
-                    'gldid'         => $row['gldid'],
-                    'glid'          => $row['glid'],
-                    'gldesc'        => $row['gldesc'],
-                    'gldate'        => $row['gldate'],
-                    'posted'        => $row['posted'],
-                    'journal_name'  => $row['journal_name'],
-                    'create_time'   => $row['create_time'],
-                    'gldoc'         => $row['gldoc'],
-                    'coacode'       => $row['coacode'],
-                    'coaname'       => $row['coaname'],
-                    'debet'         => floatval($row['debet']),
-                    'credit'        => floatval($row['credit']),
-                    'notes'         => $row['notes'],
-                    'posted_by'     => $row['posted_by'],
-                    'supp_cust'     => $row['supp_cust'],
-                    'reff_code'     => $row['reff_code'],
-                    'cost_center'   => $row['cost_center'],
-                );
-
-                $sqli = "SELECT * FROM temp_detail_jurnal WHERE 1 = 2";
-                $rsi = DB::Execute($sqli);
-                $sqli = DB::InsertSQL($rsi, $data);
-                if ($ok) $ok = DB::Execute($sqli);
-            }
-        }
-        /* E: Insert To Temp Table */
-
-        $addsql .= $optionsCabang['query'];
-
-        /* B: Showing Data From Temp Table */
-        $sql = "SELECT tmp.*, br.bid, br.branch_name
-                FROM temp_detail_jurnal tmp
-                INNER JOIN branch br ON br.branch_code = tmp.branch_code
-                WHERE 1 = 1 $addsql
-                ORDER BY tmp.gldid";
-        $rs = DB::Execute($sql);
         /* E: Showing Data From Temp Table */
 
         return $rs;
---
+++ app/routes/API/akuntingReport/summary_report/hutangSupplierAPI.php
@@ -0,0 +1,6 @@
+<?php
+
+Route::group('api/summary-report/ap-purchasing', ['middleware' => 'AuthMiddleware'], function ()
+{
+    Route::get('/excel', 'API/AkuntansiReport/SummaryReport/HutangSupplierAPI@excel')->name('api.summary_report.ap_purchasing.excel');
+});
