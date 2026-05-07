9145b6086530eb487ebfb29288899c3d5d669f81
rhmthid <rhmthid@gmail.com>
2026-05-07 15:16:09 +0700

    Update Fitur Overview Ledger Konsolidasi Part 1

--- app/models/AkuntansiReport/BukuBesarMdl.php
+++ app/models/AkuntansiReport/BukuBesarMdl.php
@@ -13,87 +13,140 @@ class BukuBesarMdl extends DB
     {
         // if (Auth::user()->pid == SUPER_USER) DB::Debug(true);
 
-        $bid = Auth::user()->branch->bid;
-
         $addsql = $sqlOpbal = "";
+        $bid = $data['bid'];
+        $status_cabang = $data['status_cabang'];
+        $status_coa = $data['status_coa'];
         $sdate = $data['sdate'];
         $edate = $data['edate'];
-        $jtid = $data['jtid'];
         $is_posted = $data['is_posted'];
         $coaid_from = $data['coaid_from'];
         $coaid_to = $data['coaid_to'];
-        $pccid = $data['pccid'];
         $with_bb = $data['with_bb'];
-
-        if ($jtid) $addsql .= " AND a.jtid = ".$jtid;
+        $record = [];
+        $addsql = "";
+        $optionsCabang = FilterCabang($bid);
 
         if ($is_posted) $addsql .= " AND a.is_posted = '$is_posted'";
 
-        if ($coaid_from != $coaid_to)
-        {
-            $rs_coa = DB::GetOne("SELECT string_agg(coacode, ';' ORDER BY coacode) FROM m_coa WHERE coaid IN (?, ?)", [$coaid_from, $coaid_to]);
+        $rs_coa = DB::GetOne("SELECT string_agg(coacode, ';' ORDER BY coacode) FROM m_coa WHERE coaid IN (?, ?)", [$coaid_from, $coaid_to]);
 
-            $data_coa = explodeData(';', $rs_coa);
-            $coacode_from = $data_coa[0];
-            $coacode_to = $data_coa[1];
-            
+        $data_coa = explodeData(';', $rs_coa);
+        $coacode_from = $data_coa[0];
+        $coacode_to = $data_coa[1];
+
+        if ($coaid_from != $coaid_to)
+        {   
             $addsql .= " AND c.coacode BETWEEN '$coacode_from' AND '$coacode_to'";
             $sqlOpbal .= " AND x.coacode BETWEEN '$coacode_from' AND '$coacode_to'";
         }
         else
         {
-            $addsql .= " AND a.coaid = ".$coaid_from;
-            $sqlOpbal .= " AND x.coaid = ".$coaid_from;
+            $addsql .= " AND c.coacode = '$coacode_from'";
+            $sqlOpbal .= " AND x.coacode = '$coacode_to'";
         }
 
-        if ($pccid) $addsql .= " AND a.pccid = ".$pccid;
+        /* B: Create Temp Table */
+        DB::Execute("DROP TABLE IF EXISTS temp_ledger");
 
-        $addsql .= " AND b.bid = ".$bid;
+        $sqli = "CREATE TEMPORARY TABLE temp_ledger (
+                    branch_code VARCHAR,
+                    gldate      TIMESTAMP,
+                    coacode     VARCHAR,
+                    coaname     VARCHAR,
+                    gldesc      VARCHAR,
+                    coa_vs      VARCHAR,
+                    gluser      VARCHAR,
+                    supp_cust   VARCHAR,
+                    openingbal  NUMERIC(18,2) DEFAULT 0,
+                    debet       NUMERIC(18,2) DEFAULT 0,
+                    credit      NUMERIC(18,2) DEFAULT 0,
+                    closingbal  NUMERIC(18,2) DEFAULT 0,
+                    gldoc       VARCHAR,
+                    reff_code   VARCHAR,
+                    gltype      VARCHAR,
+                    glnotes     VARCHAR,
+                    cost_center VARCHAR
+                ) ON COMMIT PRESERVE ROWS";
+        DB::Execute($sqli);
+        /* E: Create Temp Table */
 
-        if ($with_bb == 't')
+        /* B: Get Data PT. JKK */
+        if ($optionsCabang['conn_jkk'])
         {
-            $sqlOpbal .= " AND z.bid = ".$bid;
+            if ($with_bb == 't')
+            {
+                $opbal = "SELECT x.coaid
+                            , SUM(CASE WHEN x.default_debet = 't' THEN
+                                y.debet - y.credit
+                            ELSE
+                                y.credit - y.debet
+                            END
+                            ) AS opbal
+                        FROM general_ledger_d y
+                        JOIN general_ledger z ON z.glid = y.glid
+                        JOIN m_coa x ON x.coaid = y.coaid
+                        WHERE z.is_posted = 't' AND DATE(z.gldate) < DATE('$sdate')
+                            $sqlOpbal
+                        GROUP BY x.coaid";
+            }
+            else
+                $opbal = "SELECT x.coaid, 0 AS opbal
+                        FROM m_coa x
+                        WHERE 1 = 1 $sqlOpbal";
 
-            $opbal = "SELECT x.coaid
-                        , SUM(CASE WHEN x.default_debet = 't' THEN
-                            y.debet - y.credit
-                        ELSE
-                            y.credit - y.debet
-                        END
-                        ) AS opbal
-                    FROM general_ledger_d y
-                    JOIN general_ledger z ON z.glid = y.glid
-                    JOIN m_coa x ON x.coaid = y.coaid
-                    WHERE z.is_posted = 't' AND DATE(z.gldate) < DATE('$sdate')
-                        $sqlOpbal
-                    GROUP BY x.coaid";
+            $sql = "SELECT br.branch_code, b.gldate, c.coacode, c.coaname, b.gldesc
+                        , e.nama_lengkap, a.debet, a.credit
+                        , format_glcode(b.gldate, d.doc_code, b.gldoc, b.glid) AS doc_no
+                        , a.reff_code, d.journal_name, a.notes
+                        , (g.pcccode || ' - ' || g.pccname) AS cost_center
+
+                        -- a.gldid, b.glid, a.coaid
+                        -- , c.default_debet
+                        -- , (CASE WHEN b.jtid IN (27) THEN h.bank_nama ELSE f.nama_supp END) nama_supp
+                        -- , g.pccid, ob.opbal
+                    FROM general_ledger_d a
+                    INNER JOIN general_ledger b ON b.glid = a.glid
+                    INNER JOIN m_coa c ON c.coaid = a.coaid
+                    INNER JOIN journal_type d ON d.jtid = b.jtid
+                    INNER JOIN person e ON e.pid = b.create_by
+                    LEFT JOIN m_supplier f ON b.suppid = f.suppid -- AND b.jtid IN (4, 9, 20, 21, 22, 23, 24)
+                    LEFT JOIN profit_cost_center g ON a.pccid = g.pccid
+                    LEFT JOIN (
+                        $opbal
+                    ) ob ON a.coaid = ob.coaid
+                    LEFT JOIN m_bank h ON b.other_reff_id = h.bank_id
+                    INNER JOIN branch br ON br.bid = b.bid
+                    WHERE DATE(b.gldate) BETWEEN DATE('$sdate') AND DATE('$edate')
+                        $addsql
+                    ";
+            $rs = DB2::Execute($sql);
+
+            while (!$rs->EOF)
+            {
+                $record[] = array(
+                    'branch_code'   => $rs->fields['branch_code'],
+                    'gldate'        => $rs->fields['gldate'],
+                    'coacode'       => $rs->fields['coacode'],
+                    'coaname'       => $rs->fields['coaname'],
+                    'gldesc'        => $rs->fields['gldesc'],
+                    'gluser'        => $rs->fields['nama_lengkap'],
+                    'debet'         => floatval($rs->fields['debet']),
+                    'credit'        => floatval($rs->fields['credit']),
+                    'gldoc'         => $rs->fields['doc_no'],
+                    'reff_code'     => $rs->fields['reff_code'],
+                    'gltype'        => $rs->fields['journal_name'],
+                    'glnotes'       => $rs->fields['notes'],
+                    'cost_center'   => $rs->fields['cost_center'],
+
+                    // 'openingbal'    => floatval($rs->fields['openingbal']),
+                    // 'closingbal'    => floatval($rs->fields['closingbal']),
+                );
+
+                $rs->MoveNext();
+            }
         }
-        else
-            $opbal = "SELECT x.coaid, 0 AS opbal
-                    FROM m_coa x
-                    WHERE 1 = 1 $sqlOpbal";
-
-        $sql = "SELECT a.gldid, b.glid, b.gldate, a.coaid, c.coacode, c.coaname, d.journal_name
-                    , a.reff_code, format_glcode(b.gldate, d.doc_code, b.gldoc, b.glid) AS gldoc
-                    , c.default_debet, a.debet, a.credit, a.notes, b.gldesc, e.nama_lengkap
-        		    , (CASE WHEN b.jtid IN (27) THEN h.bank_nama ELSE f.nama_supp END) nama_supp
-                    , g.pccid, (g.pcccode || ' - ' || g.pccname) AS cost_center, ob.opbal
-                FROM general_ledger_d a
-                INNER JOIN general_ledger b ON b.glid = a.glid
-                INNER JOIN m_coa c ON c.coaid = a.coaid
-                INNER JOIN journal_type d ON d.jtid = b.jtid
-                INNER JOIN person e ON e.pid = b.create_by
-                LEFT JOIN m_supplier f ON b.suppid = f.suppid -- AND b.jtid IN (4, 9, 20, 21, 22, 23, 24)
-                LEFT JOIN profit_cost_center g ON a.pccid = g.pccid
-                LEFT JOIN (
-                    $opbal
-        		) ob ON a.coaid = ob.coaid
-        		LEFT JOIN m_bank h ON b.other_reff_id = h.bank_id
-                WHERE DATE(b.gldate) BETWEEN DATE('$sdate') AND DATE('$edate')
-                    $addsql
-                ORDER BY date(b.gldate) asc,b.gldate, d.doc_code, b.gldoc, b.glid
-                    -- format_glcode(b.gldate, d.doc_code, b.gldoc, b.glid) ASC, c.coacode";
-        $rs = DB::Execute($sql);
+        /* E: Get Data PT. JKK */
 
         return $rs;
     } /*}}}*/
